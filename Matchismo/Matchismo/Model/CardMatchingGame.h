95cee294ac52283e739c54f43c9dab6f80614b20
lainc <lain.cesar@gmail.com>
2014-07-04 15:14:07 -0700

    ff

---
+++ Matchismo/Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,38 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Martin Mandl on 06.11.13.
+//  Copyright (c) 2013 m2m server software gmbh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+
+@interface CardMatchingGame : NSObject
+
+// designated initializer
+- (instancetype)initWithCardCount:(NSUInteger)count
+                        usingDeck:(Deck *)deck;
+
+- (void)chooseCardAtIndex:(NSUInteger)index;
+- (Card *)cardAtIndex:(NSUInteger)index;
+
+- (void)drawNewCard;
+
+- (NSArray *)findCombination;
+
+@property (nonatomic, readonly) NSInteger score;
+@property (nonatomic) NSUInteger maxMatchingCards;
+@property (nonatomic, readonly) NSArray *lastChosenCards;
+@property (nonatomic, readonly) NSInteger lastScore;
+
+@property (nonatomic, readonly) NSUInteger numberOfDealtCards;
+
+@property (nonatomic, readonly) BOOL deckIsEmpty;
+
+@property (nonatomic) int matchBonus;
+@property (nonatomic) int mismatchPenalty;
+@property (nonatomic) int flipCost;
+
+@end
